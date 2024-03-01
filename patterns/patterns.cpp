8db7e5c73c411e2c738b8aa9514010ec76e46980
Anish <anisharma030@gmail.com>
2024-03-01 22:48:16 +0530

    Finished patterns

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -274,6 +274,131 @@ void p18(){
     }
 }
 
+void p19(){
+
+    int count1 = 1;
+    for (int i = 5; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        for (int k = 1; k < count1; k++)
+        {
+            cout << "  ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        count1 +=2;
+        cout << endl;
+        
+    }
+    
+    int count2 = 8;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 1; j < i+2; j++)
+        {
+            cout << "* ";
+        }
+        for (int k = 1; k <= count2; k++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j < i+2; j++)
+        {
+            cout << "* ";
+        }
+        count2 -=2;
+        cout << endl;
+        
+    }
+
+}
+
+void p20(){
+    int count1 = 8;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*" ;
+        }
+        for (int k = count1; k > 0; k--)
+        {
+            cout << " ";
+        }
+        count1-=2;
+        
+        for (int l = i; l > 0; l--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    int count2 = 2;
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 5; j > i; j--)
+        {
+            cout << "*" ;
+        }
+        for (int k = 0; k < count2; k++)
+        {
+            cout << " ";
+        }
+        count2+=2;
+        
+        for (int l = 5; l > i; l--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void p21(){
+    int rows = 4;
+    for (int i = 1; i < rows; i++)
+    {
+        if (i == 1)
+        {
+            cout << "* * * *\n";
+            continue;
+        }
+        cout << "*     *\n";
+
+        if (i == rows - 1 )
+        {
+            cout << "* * * *";
+        }
+    }
+    
+}
+
+void p22(){
+    cout << endl;
+    
+    int num = 4;
+    for (int i = 0; i < num*2 - 1 ; i++)
+    {
+        for (int j = 0; j < num*2 - 1 ; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = num*2 - 2 - j;
+            int bottom = num*2 - 2 - i;
+
+            cout << ( num - min(min(top, bottom), min(left, right)) ) << " ";
+
+        }
+        cout << endl;
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -313,6 +438,15 @@ int main(int argc, char const *argv[])
     p17();
     cout << endl;
     p18();
+    cout << endl;
+    p19();
+    cout << endl;
+    p20();
+    cout << endl;
+    p21();
+    cout << endl;
+    p22();
+    cout << endl;
 
     return 0;
 }
