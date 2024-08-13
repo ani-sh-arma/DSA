8f0ad6ad89053a3ad35cbcd702159f9e6e898e17
Anish <anisharma030@gmail.com>
2024-08-14 00:07:03 +0530

    solved longest consecutive sequence optimal approach

--- array_problems/longest_consecutive_sequence.cpp
+++ array_problems/longest_consecutive_sequence.cpp
@@ -56,16 +56,33 @@ int optimalApproach(vector<int> arr)
         return 0;
 
     unordered_set<int> ust;
-    for (int i = 0; i < arr.size(); i++)
+    int longest = 1;
+    for (int i = 0; i < n; i++)
     {
         ust.insert(arr[i]);
     }
+    for (int it : ust)
+    {
+        if (ust.find(it - 1) == ust.end())
+        {
+            int cnt = 1;
+            int x = it;
+
+            while (ust.find(x + 1) != ust.end())
+            {
+                x++;
+                cnt++;
+            }
+            longest = max(cnt, longest);
+        }
+    }
+    return longest;
 }
 
 int main(int argc, char const *argv[])
 {
-    vector<int> arr = {100, 200, 1, 3, 2, 4, 5, 6, 9, 8, 2, 8, 7, 6, 544, 58, 5, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115};
-    int res = betterApproach(arr);
+    vector<int> arr = {100, 100, 1, 3, 2, 4, 5, 6, 9, 8, 2, 8, 7, 6, 544, 58, 5, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115};
+    int res = optimalApproach(arr);
     cout << res;
     return 0;
 }
