6ff13c5ea75718ad3fc9da086959c01b46f99408
Anish <anisharma030@gmail.com>
2024-09-03 23:59:32 +0530

    solved more problems

---
+++ array_problems/find_repeating_and_missing_numbers.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> findRepeatingAndMissing(vector<int> a)
+{
+    pair<int, int> p;
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < a.size(); j++)
+        {
+            if (a[i] == a[j] && i != j)
+            {
+                p.first = a[i];
+            }
+
+            if (j + 1 != a[i] && j == a.size() - 1)
+            {
+                p.second = j + 1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    return p;
+}
+
+pair<int, int> brute(vector<int> a)
+{
+    pair<int, int> p = {-1, -1};
+    for (int i = 1; i <= a.size(); i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < a.size(); j++)
+        {
+            if (i == a[j])
+            {
+                cnt++;
+            }
+            if (cnt == 2)
+                p.first = i;
+            if (cnt == 0)
+                p.second = i;
+
+            if (p.first != -1 && p.second != -1)
+            {
+                break;
+            }
+        }
+    }
+
+    return p;
+}
+
+pair<int, int> better(vector<int> a)
+{
+    pair<int, int> p = {-1, -1};
+    int hash[a.size() + 1] = {0};
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        hash[a[i]]++;
+    }
+    for (int i = 1; i <= a.size(); i++)
+    {
+        if (hash[i] == 2)
+            p.first = i;
+        if (hash[i] == 0)
+            p.second = i;
+
+        if (p.first != -1 && p.second != -1)
+        {
+            break;
+        }
+    }
+
+    return p;
+}
+
+pair<int, int> optimal1(vector<int> a)
+{
+    long long S = 0;
+    long long Sn = 0;
+    long long S2 = 0;
+    long long S2n = 0;
+
+    for (long long i = 0; i < a.size(); i++)
+    {
+        S += a[i];
+        Sn += i + 1;
+        S2 += a[i] * a[i];
+        S2n += (i + 1) * (i + 1);
+    }
+
+    long long val1 = S - Sn;
+    long long val2 = S2 - S2n;
+
+    val2 = val2 / val1;
+
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(long long)x, (long long)y};
+}
+
+pair<int, int> optimal2(vector<int> a)
+{
+    int n = a.size();
+    int xr = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        xr = xr ^ a[i];
+        xr = xr ^ (i + 1);
+    }
+
+    // int bitNo = 0;
+    // while (1)
+    // {
+    //     if (xr & (1 << bitNo) != 0)
+    //     {
+    //         break;
+    //     }
+    //     bitNo++;
+    // }
+    int bitNo = xr & ~(xr - 1);
+
+    int zero = 0, one = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] & (1 << bitNo) != 0)
+        {
+            one = one ^ a[i];
+        }
+        else
+        {
+            zero = zero ^ a[i];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] & (1 << bitNo) != 0)
+        {
+            one = one ^ (i + 1);
+        }
+        else
+        {
+            zero = zero ^ (i + 1);
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == zero)
+        {
+            cnt++;
+        }
+    }
+    if (cnt == 2)
+    {
+        return {zero, one};
+    }
+    return {one, zero};
+}
+
+int main(int argc, char const *argv[])
+{
+    // vector<int> arr = {3, 1, 2, 5, 4, 6, 7, 4};
+    vector<int> arr = {3, 1, 2, 5, 4, 6, 7, 5};
+    pair<int, int> res = optimal2(arr);
+    cout << res.first << " " << res.second;
+    return 0;
+}
--- array_problems/merge_sorted_arrays_without_extra_space.cpp
+++ array_problems/merge_sorted_arrays_without_extra_space.cpp
@@ -1,41 +1,121 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int arr1[], int arr2[], int n, int m)
+vector<int> bruteForce(int arr1[], int arr2[], int n, int m)
 {
-    int i = n - 1, j = 0;
+    int i = 0;
+    int j = 0;
+    vector<int> arr;
 
     while (i < n && j < m)
     {
         if (arr1[i] < arr2[j])
-        {
-            i++;
-        }
+            arr.push_back(arr1[i++]);
         else
-        {
+            arr.push_back(arr2[j++]);
+    }
+
+    while (i < n)
+    {
+        arr.push_back(arr1[i++]);
+    }
+    while (j < m)
+    {
+        arr.push_back(arr2[j++]);
+    }
+    return arr;
+}
+
+void better(int arr1[], int arr2[], int n, int m)
+{
+    int i = n - 1;
+    int j = 0;
+
+    while (i >= 0 && j < m)
+    {
+        if (arr1[i] > arr2[j])
             swap(arr1[i], arr2[j]);
-            i++;
-            j++;
-        }
+        else
+            break;
+        i--;
+        j++;
     }
     sort(arr1, arr1 + n);
     sort(arr2, arr2 + m);
 }
 
-int main(int argc, char const *argv[])
+int nextGap(int gap)
+{
+    if (gap <= 1)
+        return 0;
+    return (gap / 2) + (gap % 2);
+}
+
+void optimal(int arr1[], int arr2[], int n, int m)
+{
+    int gap = nextGap(n + m); // Initial gap
+
+    // Start with the largest gap and reduce it gradually
+    while (gap > 0)
+    {
+        int i = 0, j = gap + i;
+
+        // Perform comparison and swap elements according to gap
+        while (j < n + m)
+        {
+            // Case 1: Both pointers in arr1
+            if (i < n && j < n)
+            {
+                if (arr1[i] > arr1[j])
+                {
+                    swap(arr1[i], arr1[j]);
+                }
+            }
+            // Case 2: Pointer i in arr1 and pointer j in arr2
+            else if (i < n && j >= n)
+            {
+                if (arr1[i] > arr2[j - n])
+                {
+                    swap(arr1[i], arr2[j - n]);
+                }
+            }
+            // Case 3: Both pointers in arr2
+            else if (i >= n)
+            {
+                if (arr2[i - n] > arr2[j - n])
+                {
+                    swap(arr2[i - n], arr2[j - n]);
+                }
+            }
+            i++;
+            j++;
+        }
+        gap = nextGap(gap); // Reduce the gap
+    }
+}
+
+int main()
 {
     int arr1[4] = {0, 3, 5, 7};
     int arr2[3] = {1, 4, 8};
-    merge(arr1, arr2, 4, 3);
+    // vector<int> arr = bruteForce(arr1, arr2, 4, 3);
+    // better(arr1, arr2, 4, 3);
+    optimal(arr1, arr2, 4, 3);
 
     for (int i = 0; i < 4; i++)
     {
         cout << arr1[i] << " ";
     }
+    cout << endl;
     for (int i = 0; i < 3; i++)
     {
         cout << arr2[i] << " ";
     }
+    cout << endl;
+    // for (int i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
 
     return 0;
 }
