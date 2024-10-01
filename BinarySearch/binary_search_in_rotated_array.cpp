7c7e41aa73281bf1156c7c00a4030cd0ac84e36c
Anish <anisharma030@gmail.com>
2024-10-01 13:14:52 +0530

    solved smallest value in rotated array

--- BinarySearch/binary_search_in_rotated_array.cpp
+++ BinarySearch/binary_search_in_rotated_array.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearchInRotatedArray(vector<int> a, int x)
+int binarySearchInRotatedArrayOfDistinctValues(vector<int> a, int x)
 {
     int low = 0, high = a.size() - 1;
     while (low <= high)
@@ -37,11 +37,56 @@ int binarySearchInRotatedArray(vector<int> a, int x)
     return -1;
 }
 
+bool binarySearchInRotatedArrayOfDuplicateValues(vector<int> a, int x)
+{
+    int low = 0, high = a.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (a[mid] == x)
+        {
+            return true;
+        }
+
+        if (a[low] == a[mid] && a[mid] == a[high])
+        {
+            low++;
+            high--;
+            continue;
+        }
+
+        if (a[mid] >= a[low])
+        {
+            if (a[low] <= x && x <= a[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (a[mid] <= x && x <= a[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
+
 int main(int argc, char const *argv[])
 {
-    vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
-    int x = 2;
-    int ans = binarySearchInRotatedArray(arr, x);
-    cout << "Answer : " << ans;
+    vector<int> arr = {4, 5, 6, 6, 7, 0, 1, 1, 2, 3, 4};
+    int x = 0;
+    int ans = binarySearchInRotatedArrayOfDistinctValues(arr, x);
+    cout << "Answer : " << ans << "\n";
+    cout << "Bool Answer : " << binarySearchInRotatedArrayOfDuplicateValues(arr, x) << "\n";
     return 0;
 }
---
+++ BinarySearch/smallest_value_in_rotated_array.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int smallestValueInRotatedArray(vector<int> a, int x)
+{
+    int low = 0, high = a.size() - 1;
+    int count = 0;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (a[mid] < a[mid - 1] && a[mid] < a[mid + 1])
+        {
+            cout << "if" << count++ << "\n";
+            return a[mid];
+        }
+        else if (a[mid] < a[mid - 1])
+        {
+            if (a[mid] < a[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        else
+        {
+            if (a[mid] < a[mid + 1])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int findMin(vector<int> nums)
+{
+
+    int low = 0, high = nums.size() - 1;
+    int ans = INT_MAX;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[low] <= nums[high])
+        {
+            ans = min(ans, nums[low]);
+            break;
+        }
+
+        if (nums[low] < nums[mid])
+        {
+            ans = min(ans, nums[low]);
+            low = mid + 1;
+        }
+        else
+        {
+            ans = min(ans, nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> arr = {4, 5, 6, 7, -1, 0, 1, 2, 3};
+    int ans = findMin(arr);
+    cout << "Answer : " << ans << "\n";
+    return 0;
+}
