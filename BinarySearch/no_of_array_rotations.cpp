0dc30beb0e97851f4909e4746b6103302c70771b
Anish <anisharma030@gmail.com>
2024-10-01 13:31:25 +0530

    solved number of rotations

--- BinarySearch/no_of_array_rotations.cpp
+++ BinarySearch/no_of_array_rotations.cpp
@@ -1,40 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int smallestValueInRotatedArray(vector<int> a)
+int noOfTimesRotated(vector<int> a)
 {
-    int low = 0, high = a.size() - 1;
-    while (low <= high)
+    int count = 0;
+    while (a[0] > a[a.size() - 1])
     {
-        int mid = low + (high - low) / 2;
-        if (a[mid] < a[mid - 1] && a[mid] < a[mid + 1])
-        {
-            return a[mid];
-        }
-        else if (a[mid] < a[mid - 1])
-        {
-            if (a[mid] < a[mid + 1])
-            {
-                low = mid + 1;
-            }
-            else
-            {
-                high = mid - 1;
-            }
-        }
-        else
-        {
-            if (a[mid] < a[mid + 1])
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
+        a.push_back(a.front());
+        a.erase(a.begin());
+        count++;
     }
-    return -1;
+    return count;
 }
 
 int findMin(vector<int> nums)
@@ -69,7 +45,7 @@ int findMin(vector<int> nums)
 int main(int argc, char const *argv[])
 {
     vector<int> arr = {4, 5, 6, 7, -1, 0, 1, 2, 3};
-    int ans = findMin(arr);
+    int ans = noOfTimesRotated(arr);
     cout << "Answer : " << ans << "\n";
     return 0;
 }
