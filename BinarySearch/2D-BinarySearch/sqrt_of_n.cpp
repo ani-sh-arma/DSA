3984a69dc2bd0b3afc82d17b00ca07bf431b6094
Anish <anisharma030@gmail.com>
2024-10-15 00:10:22 +0530

    solved the optimal approach of sqrt

--- BinarySearch/2D-BinarySearch/sqrt_of_n.cpp
+++ BinarySearch/2D-BinarySearch/sqrt_of_n.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int bruteForce(int n)
 {
-    long long n = 1500000341;
     long long ans = 0;
-
     for (long long i = 0; i < n; i++)
     {
         if (i * i > n)
@@ -14,8 +12,35 @@ int main(int argc, char const *argv[])
         }
         ans = i;
     }
+    return ans;
+}
 
-    cout << ans << " Actual Answer is : " << sqrt(n) << endl;
+int optimal(long long n)
+{
+    long long low = 1, high = n;
+    while (low <= high)
+    {
+        long long mid = low + (high - low) / 2;
+        long long val = mid * mid;
+
+        if (val <= (long long)(n))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return high;
+}
+
+int main(int argc, char const *argv[])
+{
+    long long n = 1500000341;
+
+    cout << optimal(n) << " Actual Answer is : " << sqrt(n) << endl;
 
     return 0;
 }
